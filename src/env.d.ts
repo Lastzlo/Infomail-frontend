3814296cb94a27495fd81cb56da58ec58efa3f56
Vlad <z.last.zlo@gmail.com>
2022-10-31 22:41:21 +0200

    add environment variables NG_APP_API_URL and NG_APP_FRONT_URL

---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+declare var process: {
+  env: {
+    NG_APP_API_URL: string;
+    NG_APP_FRONT_URL: string;
+  };
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://195.250.62.211:8028',  // product api url
-  FRONT_URL: 'http://195.250.62.211:8027' // product front url
+  API_URL: process.env.NG_APP_API_URL  || 'http://localhost:8028',
+  FRONT_URL: process.env.NG_APP_FRONT_URL || 'http://localhost:4200'
 };
-// API_URL: 'http://195.250.62.211:8028',  // product api url
-// FRONT_URL: 'http://195.250.62.211:8027' // product front url
