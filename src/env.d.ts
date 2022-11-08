31807fc14fba7b9ed2473b4b188f518196c3628e
Vlad <z.last.zlo@gmail.com>
2022-11-08 13:08:32 +0200

    ref environment vars, rename production to ENABLE_PRODUCTION_MODE, add SHOW_LOGS

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,5 +2,6 @@ declare var process: {
   env: {
     NG_APP_API_URL: string;
     NG_APP_FRONT_URL: string;
+    NG_APP_SHOW_LOGS: boolean;
   };
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  API_URL: process.env.NG_APP_API_URL  || 'http://localhost:8028',
-  FRONT_URL: process.env.NG_APP_FRONT_URL || 'http://localhost:4200'
+  ENABLE_PRODUCTION_MODE: false,                                      // turn on Angular production mode
+  API_URL: process.env.NG_APP_API_URL  || 'http://localhost:8028',    // url to the running Infomail server
+  FRONT_URL: process.env.NG_APP_FRONT_URL || 'http://localhost:4200', // url to the current
+  SHOW_LOGS: process.env.NG_APP_SHOW_LOGS || true                     // show logs in console
 };
 
 
