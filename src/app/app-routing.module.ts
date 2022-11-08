baae2502ab617086f19b1015b56ed24aad2b8f74
Vlad <z.last.zlo@gmail.com>
2022-11-08 15:38:04 +0200

    ref(app-routing.module.ts) set as default redirect to templates page after user login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
   {
     path: '', component: HomePageComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard],
     children: [
-      {path: '', pathMatch: 'full', redirectTo: 'history'},
+      {path: '', pathMatch: 'full', redirectTo: 'templates'},
       {
         path: 'tasks', children: [
           {path: '', component: TasksComponent},
